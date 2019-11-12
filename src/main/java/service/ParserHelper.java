b1dbabd5c32de79c6e9adac3d8111c1d8a19370e
Max <skaterweather41@gmail.com>
2019-11-12 12:23:47 +0200

    changed approach to parsing; results updated

--- src/main/java/service/ParserHelper.java
+++ src/main/java/service/ParserHelper.java
@@ -8,10 +8,14 @@ import org.jsoup.nodes.Node;
 import org.jsoup.select.Elements;
 
 public class ParserHelper {
-    private String queryForProductBuyBoxWrapper = "#app > section "
-            + "> section:nth-child(4) > section "
-            + "> div.sc-19tq43e-1.ewlgdA "
-            + "> div > div > div ";
+
+    private Element getProductBuyBox(Document document) {
+        return document.body().selectFirst("#app > section > section")
+                .attr("data-test-id", "StaticSplitSection")
+                .children().first().children().last().children()
+                .attr("data-test-id", "BuyBox")
+                .first().children().first().children().first();
+    }
 
     public Elements getMainContainerRows(Document document) {
         String queryForMainContainerGrid = "#app > section "
@@ -22,35 +26,23 @@ public class ParserHelper {
     }
 
     public String getProductName(Document document) {
-        String queryForName = queryForProductBuyBoxWrapper
-                + "> div.sc-1ybtkva-0.PQpcS "
-                + "> div.iay39c-1.faQObc";
-        return document.body().selectFirst(queryForName).text();
+        return getProductBuyBox(document)
+                .children().get(1)
+                .children().last().text();
     }
 
     public String getProductBrand(Document document) {
-        String queryForBrandImage = queryForProductBuyBoxWrapper
-                + "> div.sc-1ybtkva-0.PQpcS "
-                + "> div:nth-child(1) > a > img";
-         return document.body().selectFirst(queryForBrandImage).attributes().get("alt");
+         return getProductBuyBox(document)
+                 .children().get(1)
+                 .selectFirst("img").attributes().get("alt");
     }
 
     public String getProductBasePrice(Document document) {
-        String queryForPriceField = queryForProductBuyBoxWrapper
-                + "> div.sc-12rq7nw-0.hQOIjj.sc-1t8547r-1.iuFFwF"
-                + "> div.sc-1t8547r-0.shMKG";
-        Elements priceField = document.body().selectFirst(queryForPriceField).children();
-        if (priceField.size() == 2) {
-            return priceField.get(1).text();
-        }
-        return priceField.first().text();
+        return getProductBuyBox(document).children().get(2).select("span").last().text();
     }
 
     public String getProductSalePrice(Document document) {
-        String queryForPriceField = queryForProductBuyBoxWrapper
-                + "> div.sc-12rq7nw-0.hQOIjj.sc-1t8547r-1.iuFFwF"
-                + "> div.sc-1t8547r-0.shMKG";
-        Elements priceField = document.body().selectFirst(queryForPriceField).children();
+        Elements priceField = getProductBuyBox(document).children().get(2).select("span");
         if (priceField.size() == 2) {
             return priceField.first().text();
         }
@@ -58,10 +50,17 @@ public class ParserHelper {
     }
 
     public Elements getProductColorLinks(Document document) {
-        String queryForColorsList = queryForProductBuyBoxWrapper
-                + "> div.sc-12rq7nw-3.ivQbqZ"
-                + "> div.sc-1befil5-0.eROFI";
-        return document.body().selectFirst(queryForColorsList).select("a");
+        Elements colorLinks = getProductBuyBox(document)
+                .children().get(4)
+                .children().first().children();
+        //check if there's button -> then there's list
+        if (colorLinks.select("button").size() > 0) {
+            return colorLinks.first().children().first()
+                    .children().first()
+                    .children().not("button").first()
+                    .children().first().children().select("a");
+        }
+        return colorLinks;
     }
 
     public String getProductArticle(Document document) {
